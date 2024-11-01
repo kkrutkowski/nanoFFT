5cc8a2716fb42f5caf10d025d5ec1468656251d1
Kacper Rutkowski <kk.rutkowski@student.uw.edu.pl>
2024-11-01 22:40:36 +0100

    Add files via upload

---
+++ src/nanofft.c
@@ -0,0 +1,45 @@
+#define FLOAT float
+
+#include <math.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <complex.h>
+
+#include "./cobra.h"
+
+// Helper function to check if N is a power of two
+bool is_power_of_two(int N) {
+    return (N > 0) && ((N & (N - 1)) == 0);
+}
+
+
+const complex FLOAT* generate_buffer(int N){
+    complex FLOAT* buffer = (complex FLOAT*) malloc(N * sizeof(complex FLOAT));
+    int shift = 0;
+    for (int step = N; step > 1; step >>= 1) { // Right bit shift for division by 2
+        int half_step = step >> 1; // Right bit shift for division by 2
+            for (int j = 0; j < half_step; j++) {
+                buffer[shift + j] = cexp(-2.0 * I * M_PI * j / step);
+                //printf("%i\t%i\n", N, shift+j);
+            }
+        shift += step >> 1;
+    }
+return buffer;}
+
+
+void sande_tukey_in_place(complex FLOAT *signal, const complex FLOAT *buffer, int N) {
+    int shift = 0;
+    for (int step = N; step > 1; step >>= 1) { // Right bit shift for division by 2
+        int half_step = step >> 1; // Right bit shift for division by 2
+        for (int i = 0; i < N; i += step) {
+            for (int j = 0; j < half_step; j++) {
+                complex FLOAT even = signal[i + j];
+                complex FLOAT odd = signal[i + j + half_step];
+                signal[i + j] = even + odd;
+                signal[i + j + half_step] = (even - odd) * buffer[shift + j]; //* cexp(-2.0 * I * M_PI * j / step);
+            }
+        }
+        shift += step >> 1;
+    }
+}
