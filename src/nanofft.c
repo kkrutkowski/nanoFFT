4a52a863b754c3d60cf89a71a2d278ba4787dacf
Kacper Rutkowski <kk.rutkowski@student.uw.edu.pl>
2024-11-06 00:30:11 +0100

    Add const twiddles for vectorized finalization

--- src/nanofft.c
+++ src/nanofft.c
@@ -40,8 +40,16 @@ static inline uint32_t intmin(uint32_t a, uint32_t b) {return (a < b) ? a : b;}
                     {.i = {0, 1, 4, 5, 2, 3, 6, 7}},
                     {.i = {7, 5, 4, 1, 6, 4, 2, 0}}};
 
-                    typedef union {__m256 m256; float i[8];} m256_union;
+                    typedef union {__m256 m256; float f[8];} m256_union;
                     // Finalization twiddles
+                    static const m256_union real_twiddles[3] __attribute__((aligned(64))) = {
+                    {.f = {1.0f, M_SQRT1_2, 0.0f, -M_SQRT1_2, 1.0f, M_SQRT1_2, 0.0f, -M_SQRT1_2}},
+                    {.f = {1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f}},
+                    {.f = {1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f}}};
+                    static const m256_union imag_twiddles[3] __attribute__((aligned(64))) = {
+                    {.f = {0.0f, M_SQRT1_2, 1.0f, M_SQRT1_2, 0.0f, M_SQRT1_2, 1.0f, M_SQRT1_2}},
+                    {.f = {0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f}},
+                    {.f = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f}}};
 
                 static inline void nanofft_mm256_shuffle(__m256 *a, __m256 *b){
                     __m256 tmp = _mm256_set_m128(_mm256_extractf128_ps(*b, 0), _mm256_extractf128_ps(*a, 0));
